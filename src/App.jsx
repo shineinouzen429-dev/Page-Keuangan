c0076f20ee46a03b7cc8993f90080adab50b7d49
Ayyas <shineinouzen429@gmail.com>
2025-10-09 15:08:54 +0700

    Tugas Page-keuangan

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div
+      className="h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center"
+      style={{
+        backgroundImage:
+          "url('https://i.pinimg.com/1200x/55/c8/1d/55c81daaf67c75fdf9b9b77f18639661.jpg')",
+      }}
+    >
+      <h1 className="text-white text-7xl font-bold drop-shadow-lg">
+        
+      </h1>
+    </div>
+  );
+}
+
+export default App;
